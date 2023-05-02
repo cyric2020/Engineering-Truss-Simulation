46ec27d147dbd3b5c6b535b3a2936413f070186d
cyric2020 <60243062+cyric2020@users.noreply.github.com>
2023-05-02 16:49:33 +1000

    fixed udl

--- view_truss.py
+++ view_truss.py
@@ -55,10 +55,13 @@ truss = Truss()
 # truss.loadData('base_trusses/warren.yaml')
 
 # Trusses for testing
-# truss.loadData('testing_trusses/warren_flat.yaml')
+truss.loadData('testing_trusses/warren_flat.yaml')
 # truss.loadData('testing_trusses/pratt_flat_w_pylon.yaml')
 # truss.loadData('testing_trusses/pratt_rise.yaml')
-truss.loadData('testing_trusses/warren_rise.yaml')
+# truss.loadData('testing_trusses/warren_rise.yaml')
+
+# Truss for verification
+# truss.loadData('verification_trusses/small_truss.yaml')
 
 # truss.loadState('example_trusses/warren_big_middle_support.yaml')
 
